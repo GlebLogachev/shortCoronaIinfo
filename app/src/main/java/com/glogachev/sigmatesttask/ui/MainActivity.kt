e5700a33624ab1f95991a826604cf1e38797b0d9
GlebLogachev <GlebLogachev@users.noreply.github.com>
2021-11-27 14:28:19 +0700

    dependencies

--- app/src/main/java/com/glogachev/sigmatesttask/ui/MainActivity.kt
+++ app/src/main/java/com/glogachev/sigmatesttask/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.glogachev.sigmatesttask
+package com.glogachev.sigmatesttask.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.glogachev.sigmatesttask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
